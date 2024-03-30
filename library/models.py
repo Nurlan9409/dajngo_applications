fb65385547eb380156609d5e802c7ba3cc71382f
John Smith <nurlanmamitov@gmail.com>
2024-03-30 22:44:20 +0500

    orm django

--- library/models.py
+++ library/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+     name = models.CharField(max_length=50)
+     description = models.TextField()
+     price = models.FloatField()
+     count = models.IntegerField(default=1)
